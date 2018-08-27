fa4a11e3340c3bb015701c09cf4cb73411c4b9b0
Ashley Singleton <asingleton6@gmail.com>
2018-08-27 12:26:13 -0400

    Restaurant listing style clean up

--- js/main.js
+++ js/main.js
@@ -143,22 +143,26 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const wrapper = document.createElement('div');
+  wrapper.className = 'restaurant-text-wrapper';
+  li.append(wrapper)
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  wrapper.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  wrapper.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  wrapper.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  wrapper.append(more)
 
   return li
 }
