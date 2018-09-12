948df887148ca12d66dfe157b4063335ec8a1db9
Ashley Singleton <asingleton6@gmail.com>
2018-09-11 23:58:02 -0400

    Register service worker.

---
+++ js/register.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function() {
+    console.log("Service Worker registration worked.");
+  }).catch(function() {
+    console.log("Service worker registration failed.");
+  });
+}
